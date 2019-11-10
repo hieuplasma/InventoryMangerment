fbe93ffa3d76a4399a138e0df39c1c07e7121082
tranhieutuyetmai@gmail.com <tranhieutuyetmai@gmail.com>
2019-11-10 08:02:22 +0700

    Updates

---
+++ precache-manifest.712d450a80116ad68d2a65addcd25562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5394af4440ce2a9d0ee86e8fabe601e5",
+    "url": "/hieuplasma/InventoryMangerment/index.html"
+  },
+  {
+    "revision": "f4741c4592fb9ea508b0",
+    "url": "/hieuplasma/InventoryMangerment/static/css/main.42232a5a.chunk.css"
+  },
+  {
+    "revision": "e288c6ec719fb2d0ab66",
+    "url": "/hieuplasma/InventoryMangerment/static/js/2.bee47f9f.chunk.js"
+  },
+  {
+    "revision": "f4741c4592fb9ea508b0",
+    "url": "/hieuplasma/InventoryMangerment/static/js/main.0b1db437.chunk.js"
+  },
+  {
+    "revision": "4f25bd0e33a52dafd10d",
+    "url": "/hieuplasma/InventoryMangerment/static/js/runtime-main.42417d68.js"
+  },
+  {
+    "revision": "cb43de8cb1c230e3994d5556760c13de",
+    "url": "/hieuplasma/InventoryMangerment/static/media/jerry.cb43de8c.jpg"
+  }
+]);
